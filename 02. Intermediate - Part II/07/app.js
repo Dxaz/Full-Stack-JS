955c476b3e55f8ab8a0b80928c4e8848cc5e2e2b
Dxaz <59984551+Dxaz@users.noreply.github.com>
2024-04-13 22:19:54 -0500

    Learning Destructuring

--- 02. Intermediate - Part II/07/app.js
+++ 02. Intermediate - Part II/07/app.js
@@ -1,12 +1,16 @@
 //
 // -----------------------------
-// 
+//  7. Destructuring
 // -----------------------------
-// 
-// 
-// 
+// Destructuring allows us to "unpack" values from
+// data-structures (Arrays, objects) into seperate
+// distinct variables
 // -----------------------------
-// 
-
+// Arrays
+// -----------------------------
+const foo = [1,2,3]
+console.log(foo[0]);
+console.log(foo[1]);
+console.log(foo[2]);
 
 // challenge given
