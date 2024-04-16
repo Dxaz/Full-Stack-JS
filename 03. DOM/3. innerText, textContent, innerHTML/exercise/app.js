798cc5aee0390ea1182731887afaf5e9fb3aa1d6
Dxaz <diaz.gregory@outlook.com>
2024-04-16 18:37:59 -0500

    Finished innerHTML, innerText, and textContent

--- 03. DOM/3. innerText, textContent, innerHTML/exercise/app.js
+++ 03. DOM/3. innerText, textContent, innerHTML/exercise/app.js
@@ -1,3 +1,14 @@
 // 1. Check the innerText of "first" element
+let first = document.querySelector(".first")
+console.log(first.innerText)
+first.innerText = "This is the first text and I'm changing it via javascript"
+
 // 2. Check the textContent of "second" element
+let second = document.querySelector(".second")
+console.log(second.textContent);
+second.textContent = "This is the new text content"
+
 // 3. Check the innerHTML of "third" element
+let third = document.querySelector(".third")
+console.log(third.innerHTML);
+third.innerHTML = "<span>This is now how things work. I'm in control. I do what <em>I</em> <h1>WANT!!!!!</h1></span>"
--- 03. DOM/3. innerText, textContent, innerHTML/live coding/app.js
+++ 03. DOM/3. innerText, textContent, innerHTML/live coding/app.js
@@ -1,3 +1,19 @@
+const p = document.querySelector('p')
+
 // innerText
+console.log(p.innerText);
+//----
+// change text
+// const h1 = document.querySelector("h1");
+// h1.innerText = "Changed"
+//----
+// change html 
+const h1 = document.querySelector("h1");
+h1.innerHTML = "<em>Greg Waz Here</em>"
+//----
+
 // textContent
+console.log(p.textContent);
+
 // innerHTML
+console.log(p.innerHTML);
