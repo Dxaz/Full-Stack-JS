1503396fc0deb2ef01e2a79572c5c4fb0e79a99f
Dxaz <diaz.gregory@outlook.com>
2024-04-16 18:46:23 -0500

    Finished Classlist

--- 03. DOM/4. Classlist/app.js
+++ 03. DOM/4. Classlist/app.js
@@ -1,4 +1,18 @@
+const h1 = document.querySelector('h1')
+
 // classlist
+console.log(h1.classList);
+
 // add()
+h1.classList.add("new-cls");
+
 // remove()
+h1.classList.remove("styles");
+h1.classList.remove("new-cls");
+
 // toggle()
+h1.classList.toggle("apple");
+h1.classList.toggle("styles");
+// h1.classList.toggle("styles")
+
+
