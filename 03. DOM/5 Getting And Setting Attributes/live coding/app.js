075a0884fcc5a000118e8ffe7ff4ca9a0d285ba9
Dxaz <diaz.gregory@outlook.com>
2024-04-16 19:47:25 -0500

    Finished getAttribute and setAttribute

--- 03. DOM/5 Getting And Setting Attributes/live coding/app.js
+++ 03. DOM/5 Getting And Setting Attributes/live coding/app.js
@@ -1,5 +1,25 @@
+const a = document.querySelector("a")
+const input = document.querySelector("input")
+
 // href
+console.log(a.href);
+a.href = "https://youtube.com"
+a.href="https://google.com/"
+
 // value
+console.log(input.value);
+// input.value = "value"
+// input.placeholder = "please provide a strong password"
+
 // type
+// input.type ="password"
+console.log(input.type);
+
 // getAttribute(attrName)
+console.log(input.getAttribute("placeholder"));
+
 // setAttribute(attrName, value)
+input.setAttribute("placeholder", "Password")
+input.setAttribute("type", "password")
+
+
