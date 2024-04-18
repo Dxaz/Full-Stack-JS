e1fa2b8e1749209b05462d796aa7086e0ef9b398
Dxaz <diaz.gregory@outlook.com>
2024-04-17 19:04:39 -0500

    Finished Creating and Removing Elements

--- 03. DOM/8. createElement, append, prepend, inertAdject, removeChild, remove/exercise/app.js
+++ 03. DOM/8. createElement, append, prepend, inertAdject, removeChild, remove/exercise/app.js
@@ -1,6 +1,19 @@
 // createElement()
+const h1 = document.createElement("h1")
+h1.innerText = "InnerText"
 // append()
+const body = document.querySelector("body")
+body.append(h1)
 // prepend()
+const ul = document.createElement("ul")
+const liTwo = document.createElement("li")
+const li = document.createElement("li")
+li.innerText = "I was put second"
+liTwo.innerText = "I was put first"
+ul.appendChild(liTwo)
+body.append(ul)
+ul.prepend(li)
+
 
 /* insertAdjacentElement(
     'beforebegin'
@@ -10,5 +23,14 @@
     element
 ) */
 
+const liThree = document.createElement("li")
+liThree.innerText = "adjacentElement"
+li.insertAdjacentElement("beforeBegin", liThree)
+li.insertAdjacentElement("afterend", liThree)
+li.insertAdjacentElement("beforeEnd", liThree)
+li.insertAdjacentElement("afterEnd", liThree)
+
 // removeChild()
+ul.removeChild(liThree)
 // remove()
+ul.remove()
--- 03. DOM/8. createElement, append, prepend, inertAdject, removeChild, remove/live coding/app.js
+++ 03. DOM/8. createElement, append, prepend, inertAdject, removeChild, remove/live coding/app.js
@@ -16,3 +16,45 @@
 // removeChild()
 // remove()
 // ------------------------------------
+// Creating Element
+// const h1 = document.createElement("h1")
+// const body = document.body
+// h1.textContent = "Hello World"
+// h1.classList.add("greetings")
+// h1.classList.remove("greetings")
+// body.appendChild(h1)
+// console.log(h1);
+// ------------------------------------
+// const ul = document.querySelector('ul')
+// const newLi = document.createElement("li")
+// newLi.innerText = "I'm li tag"
+// ul.appendChild(newLi)
+
+// const firstLi = document.querySelector('li')
+// // selector.insertBefore(what,where)
+// ul.insertBefore(newLi, firstLi)
+// ------------------------------------
+// const firstP = document.querySelector("p")
+// const i = document.createElement("i")
+// i.innerText = "I'm Italics"
+// i.style.color = "skyblue"
+// firstP.insertAdjacentElement("beforeBegin",i)
+// firstP.insertAdjacentElement("afterBegin",i)
+// firstP.insertAdjacentElement("beforeEnd",i)
+// firstP.insertAdjacentElement("afterEnd",i)
+// ------------------------------------
+// let section = document.querySelector("section")
+// const i = document.createElement("i")
+// i.innerText = "I'm Italics"
+// i.style.color = "skyblue"
+
+// const span = document.createElement("span")
+// span.innerText = "Greg"
+// span.style.color = "crimson"
+// section.append(i,span);
+// section.prepend(i,span);
+// ------------------------------------
+const newList = document.querySelector(".new-list")
+const fourth = document.querySelector(".fourth")
+newList.removeChild(fourth)
+newList.remove()
