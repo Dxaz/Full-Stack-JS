99a1395815a42e77bb9e8734e4db4510498a733b
Dxaz <diaz.gregory@outlook.com>
2024-04-17 19:31:32 -0500

    Finished Events

--- 03. DOM/9. 3 ways to add events/app.js
+++ 03. DOM/9. 3 ways to add events/app.js
@@ -1,5 +1,23 @@
 // ----------- BAD WAY-----------
+// const secondBtn = document.querySelector(".second-btn")
+// secondBtn.onclick = function() {
+//     console.log("Greg was here");
+// };
+
 
 // ----------- GREAT WAY-----------
+// const best = document.querySelector(".best")
+
+// function greetings(){
+//     console.log("Hello World");
+// }
+
+// best.addEventListener("click", () => console.log("Yellow"))
 
 // ----------- Event (e) Object -----------
+const form = document.querySelector('form')
+const input = document.querySelector("input")
+form.addEventListener('input', (event)=>{
+    event.preventDefault()
+    console.log(event.data);
+})
