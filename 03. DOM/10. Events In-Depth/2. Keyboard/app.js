ace62eb00626417010d094a3dec11a865e8afcd4
Dxaz <diaz.gregory@outlook.com>
2024-04-19 16:44:29 -0500

    Finshed Keyboard Events

--- 03. DOM/10. Events In-Depth/2. Keyboard/app.js
+++ 03. DOM/10. Events In-Depth/2. Keyboard/app.js
@@ -1,2 +1,15 @@
 // https://www.w3schools.com/jsref/obj_keyboardevent.asp
 const input = document.querySelector("input");
+
+// input.addEventListener("keypress", (e)=>console.log("Key Pressed"))
+// input.addEventListener("keyup", (e)=>console.log("Key Up"))
+// input.addEventListener("keydown", (e)=>console.log("Key Down"))
+
+
+// Useful properties and methods
+input.addEventListener('keypress', (e) => {
+    console.log(e.keyCode)
+    console.log(e.Code)
+    console.log(e.ctrlKey)
+    console.log(e.shiftKey)
+})
