4cb8a72a9b095576ac77b42dde07b40f152d34e0
Dxaz <diaz.gregory@outlook.com>
2024-04-23 21:00:41 -0500

    Starting constructor functions.

---
+++ 04. Advanced - Part III/01. OOP/03. Constructor function/live coding/app.js
@@ -0,0 +1,4 @@
+// Constructor functions in JavaScript are regular functions used with the new
+// keyword to create and initialize objects with shared properties and methods.
+// They act as blueprints for creating multiple instances of objects with the
+// same structure and behavior.
